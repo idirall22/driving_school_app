f3bd602d052e658f2cac4a93deaabd15e678b85b
idir <idirall22@gmail.com>
2019-08-01 18:50:11 +0100

    I deleted the setNextExamLevel function

--- backend/exam_list_utils.go
+++ backend/exam_list_utils.go
@@ -14,35 +14,12 @@ func checkIfExamListIsValid(examList *ExamList) bool {
 	}
 
 	if examList.Archived {
-		if examList.DateExam.Day() <= time.Now().Day() {
+		if examList.DateExam.Day() >= time.Now().Day() {
 			return true
 		}
 		return false
 	}
 	return true
-}
-
-// Set next exam level
-func setNextExam(dateExam time.Time, lastExamDate *time.Time,
-	LastExamStatus, status bool, nextExam uint8) uint8 {
-
-	// When the examList is archived first time
-	if lastExamDate == nil {
-		if status {
-			return nextExam + 1
-		}
-	} else {
-		if dateExam.Unix() == lastExamDate.Unix() {
-			if LastExamStatus {
-				nextExam--
-			}
-			if status {
-				nextExam++
-			}
-			return nextExam
-		}
-	}
-	return nextExam
 }
 
 // Return an examList model from a map
